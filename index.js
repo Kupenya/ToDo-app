ccd93eb91fccd8bb9672f6acf0665464da3655ea
Kupenya <susanphonsus@gmail.com>
2023-06-14 15:14:19 +0100

    more functionality

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 // Get DOM elements
-const todoForm = document.getElementById('todo-form');
-const todoInput = document.getElementById('todo-input');
-const todoList = document.getElementById('todo-list');
+const todoForm = document.getElementById("todo-form");
+const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
+const clearBtn = document.getElementById("clear-btn");
+const filterBtns = document.querySelectorAll(".filter-btn");
 
 // Store TODO items
 let todos = [];
 
 // Function to render TODO items
 function renderTodos() {
-  todoList.innerHTML = '';
+  todoList.innerHTML = "";
 
   todos.forEach((todo, index) => {
-    const li = document.createElement('li');
-    const completedClass = todo.completed ? 'completed' : '';
+    const li = document.createElement("li");
+    const completedClass = todo.completed ? "completed" : "";
     li.innerHTML = `
+    <span class="todo-number">${index + 1}.</span>
       <span class="todo-text ${completedClass}">${todo.title}</span>
-      <span class="todo-delete">X</span>
-      <input type="checkbox" class="todo-checkbox" data-index="${index}" ${todo.completed ? 'checked' : ''}>
+      <div class="todo-actions">
+        <button class="edit-btn">Edit</button>
+        <span class="todo-delete">X</span>
+      </div>
+      <input type="checkbox" class="todo-checkbox" data-index="${index}" ${
+      todo.completed ? "checked" : ""
+    }>
     `;
     todoList.appendChild(li);
   });
@@ -26,10 +34,10 @@ function renderTodos() {
 function addTodo() {
   const todoText = todoInput.value.trim();
 
-  if (todoText !== '') {
+  if (todoText !== "") {
     todos.push({ title: todoText, completed: false });
     renderTodos();
-    todoInput.value = '';
+    todoInput.value = "";
   }
 }
 
@@ -39,23 +47,108 @@ function deleteTodo(index) {
   renderTodos();
 }
 
+// Function to toggle todo completion
+function toggleTodoComplete(index) {
+  todos[index].completed = !todos[index].completed;
+  renderTodos();
+}
+
+// Function to edit a todo
+function editTodo(index, newTitle) {
+  todos[index].title = newTitle;
+  renderTodos();
+}
+
+// Function to clear completed todos
+function clearCompletedTodos() {
+  todos = todos.filter((todo) => !todo.completed);
+  renderTodos();
+}
+
+// Function to filter todos
+function filterTodos(filter) {
+  const activeBtn = document.querySelector(".filter-btn.active");
+  if (activeBtn) {
+    activeBtn.classList.remove("active");
+  }
+
+  const btn = document.querySelector(`.filter-btn[data-filter="${filter}"]`);
+  btn.classList.add("active");
+
+  switch (filter) {
+    case "active":
+      renderFilteredTodos(false);
+      break;
+    case "completed":
+      renderFilteredTodos(true);
+      break;
+    default:
+      renderTodos();
+      break;
+  }
+}
+
+// Function to render filtered todos
+function renderFilteredTodos(completed) {
+  const filteredTodos = todos.filter((todo) => todo.completed === completed);
+  todoList.innerHTML = "";
+
+  filteredTodos.forEach((todo, index) => {
+    const li = document.createElement("li");
+    const completedClass = todo.completed ? "completed" : "";
+    li.innerHTML = `
+    <span class="todo-number">${index + 1}.</span>
+      <span class="todo-text ${completedClass}">${todo.title}</span>
+      <div class="todo-actions">
+        <button class="edit-btn">Edit</button>
+        <span class="todo-delete">X</span>
+      </div>
+      <input type="checkbox" class="todo-checkbox" data-index="${index}" ${
+      todo.completed ? "checked" : ""
+    }>
+    `;
+    todoList.appendChild(li);
+  });
+}
+
 // Event listeners
-todoForm.addEventListener('submit', function(event) {
+todoForm.addEventListener("submit", function (event) {
   event.preventDefault();
   addTodo();
 });
 
-todoList.addEventListener('click', function(event) {
-  if (event.target.classList.contains('todo-delete')) {
-    const li = event.target.parentElement;
+todoList.addEventListener("click", function (event) {
+  if (event.target.classList.contains("todo-delete")) {
+    const li = event.target.parentElement.parentElement;
     const index = Array.from(todoList.children).indexOf(li);
     deleteTodo(index);
   }
-  if (event.target.classList.contains('todo-checkbox')) {
+
+  if (event.target.classList.contains("todo-checkbox")) {
     const index = event.target.dataset.index;
-    todos[index].completed = event.target.checked;
-    renderTodos();
+    toggleTodoComplete(index);
   }
+
+  if (event.target.classList.contains("edit-btn")) {
+    const li = event.target.parentElement.parentElement;
+    const index = Array.from(todoList.children).indexOf(li);
+    const todoText = li.querySelector(".todo-text").textContent;
+    const newTitle = prompt("Edit Todo:", todoText);
+    if (newTitle && newTitle.trim() !== "") {
+      editTodo(index, newTitle.trim());
+    }
+  }
+});
+
+clearBtn.addEventListener("click", function () {
+  clearCompletedTodos();
+});
+
+filterBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const filter = btn.dataset.filter;
+    filterTodos(filter);
+  });
 });
 
 // Initial rendering
